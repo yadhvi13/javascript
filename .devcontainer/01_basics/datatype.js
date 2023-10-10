cf6ecdd92eb1d809d6405dd3785d8adf804c794d
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2023-10-10 18:40:07 +0000

    done

---
+++ .devcontainer/01_basics/datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version 
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+let name = "yogeeta" // string//
+let age = 18 // numbers//
+let isLoggedIn = false // boolean//
+let state; // undefined //
+
+/*
+number => 2 to power 53
+bigint
+string "" or ''
+boolean => true or false
+null => standalone value
+undefined => khali hona
+symbol => used for uniqueness
+object
+*/
+
+console.log(typeof "yogeeta");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
--- .devcontainer/variables.js
+++ .devcontainer/variables.js
@@ -2,7 +2,17 @@ const accountId = 144553
 let accountEmail = "yogeeta@googole.com"
 var accountPassowrd = "12345"
 accountCity = "Jaipur"
+let accountState;
 
-accountId = 2
+//accountId = 2//no allowed
 
+accountEmail = "hc@hc.com"
+accountPassword = "121234"
+accountCity = "bangalore"
 console.log(accountId);
+
+/*  prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
