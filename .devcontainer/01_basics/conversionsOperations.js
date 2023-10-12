66cdfe602a9ce67927416468ce963fcbbbc544bb
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2023-10-12 15:12:30 +0000

    done

---
+++ .devcontainer/01_basics/conversionsOperations.js
@@ -0,0 +1,12 @@
+let score = true
+//console.log(typeof score);//
+//console.log(typeof(score));//
+
+let valueInNumber = true(score)
+//console.log(typeof valueInNumber);//
+//console.log(valueInNumber); //
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) ;
