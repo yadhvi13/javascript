3588b1b3c905e2046b3eefec030f0b016356c2d2
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2023-12-31 15:05:52 +0000

    variables.js 01/basics

--- .devcontainer/01_basics/variables.js
+++ .devcontainer/01_basics/variables.js
@@ -16,7 +16,7 @@ because of issue in block scope and functional scope
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 */
-
+/*
 const id = Symbol('123');
 const anotherId= Symbol('123');
 
@@ -28,5 +28,23 @@ const myFunction = function(){
     console.log("hello world");
 }
 const heros = ["shaktiman","Naagraj","Doga"];
-console.log(typeof anotherId);
+console.log(typeof anotherId);  */
 
+//**********************************************/
+/* stack memory(PRIMITIVE DATATYPES) and heap memory(NON-PRIMITIVE)*/
+ let myYoutubename = "hiteshchaoudharydotcom"
+
+ let anothername = "chaiaurcode"
+ console.log(anothername);
+ console.log(myYoutubename);
+
+
+ let user = {
+    email: "user@google.com",
+    upi: "user@ybl",
+ }
+ let usertwo = user
+
+ usertwo.email = "hitesh@google.com"
+ console.log(user.email);
+ console.log(usertwo.email);
