2136392660201d3dbb3448402ebe19fe3103e032
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2023-12-31 14:23:26 +0000

    variables.js

--- .devcontainer/variables.js
+++ .devcontainer/variables.js
@@ -1,4 +1,4 @@
-const accountId = 144553
+/* const accountId = 144553
 let accountEmail = "yogeeta@googole.com"
 var accountPassowrd = "12345"
 accountCity = "Jaipur"
@@ -13,6 +13,20 @@ console.log(accountId);
 
 /*  prefer not to use var 
 because of issue in block scope and functional scope
-*/
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+*/
+
+const id = Symbol('123');
+const anotherId= Symbol('123');
+
+console.log(id === anotherId);
+
+
+const outsideTemp = null;
+const myFunction = function(){
+    console.log("hello world");
+}
+const heros = ["shaktiman","Naagraj","Doga"];
+console.log(typeof anotherId);
+
