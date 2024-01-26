ebd0b84ebddd22a96aa02225676be7635647867f
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2024-01-26 15:11:43 +0000

    array

---
+++ .devcontainer/01_basics/Array.js
@@ -0,0 +1,32 @@
+// elements or Array of javascript is resizeable 
+/* const myArr = [0,1,2,3,4,5]
+const myHeros = ["shaktiman","naagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+*/
+
+// Array method
+/* const myArr = [0,1,2,3,4,5]
+myArr.push(6)               //element is adding into array from end 
+myArr.push(7)               //element is adding into array 
+myArr.pop()                 //last element is removing from  array    
+*/
+
+const myArr = [0,1,2,3,4,5]
+//myArr.unshift(9)       //element is adding into array from starting   
+//myArr.shift()
+
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(3));
+
+//const newArr = myArr.join()
+//console.log(myArr);
+//console.log(typeof newArr);
+//console.log(newArr);
+
+// slice,and splice
+console.log("A ",myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B ",myArr);
---
+++ .devcontainer/01_basics/dates.js
@@ -0,0 +1,21 @@
+/* let mydate = new Date()
+ console.log(mydate);
+ console.log(mydate.toString());
+ console.log(mydate.toDateString());
+ console.log(mydate.toLocaleDateString());
+ console.log(mydate.toJSON());
+
+ console.log(typeof mydate)
+*/
+/*
+ let mycreateddate = new Date(2023, 0, 23) 
+ let mycreateddate = new Date(2023, 0, 23, 5, 3)
+ let mycreateddate = new Date("2023-01-14")*/
+ let mycreateddate = new Date("01-14-2023") 
+// console.log(mycreateddate.toLocaleString());
+
+let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+//console.log(mycreateddate.getTime());
+console.log(Date.now()/1000);
+
