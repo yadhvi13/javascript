15b5a04566eeb521b2ae32391664724432fa4b86
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2024-01-27 02:40:18 +0000

    array

--- .devcontainer/01_basics/Array.js
+++ .devcontainer/01_basics/Array.js
@@ -26,7 +26,16 @@ const myArr = [0,1,2,3,4,5]
 //console.log(newArr);
 
 // slice,and splice
-console.log("A ",myArr);
+/* console.log("A ",myArr);
+
 const myn1 = myArr.slice(1,3)
+
 console.log(myn1);
 console.log("B ",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("c",myArr);
+console.log(myn2);
+*/
+
+// Slice print original array and splice manipulates array with given range
---
+++ 02_basics/Array2.js
@@ -0,0 +1,5 @@
+const marvel_heros = ["thor","ironman","spiderman"]
+const dc_heros = ["superman","flash","Batman"]
+
+marvel_heros.push(dc_heros)
+console.log(marvel_heros);
