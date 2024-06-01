685ff58ba58b03568239e9ea8faba8850592eaed
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2024-06-01 16:24:17 +0000

    objects_3.js

---
+++ 02_basics/objects_3.js
@@ -0,0 +1,12 @@
+const course = {
+    coursename : "javascipt",
+    price : "999",
+    courseInstructor: "yogeeta mam",
+}
+//course.courseInstructor
+const{courseInstructor : tutor} = course
+console.log(tutor);
+
+//----> destructure of objects
+
+// concepts of APIs 
