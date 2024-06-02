3bffd597c922e86681c8bb68ce8d1a7ffff0b16d
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2024-06-02 12:56:38 +0000

    03_basics

---
+++ 03_basics/functions.js
@@ -0,0 +1,25 @@
+
+//function SayMyName(){
+//    console.log("h");
+ //   console.log("i");
+//    console.log("t");
+//    console.log("e");
+//    console.log("s");
+//    console.log("h");
+//}
+//SayMyName()
+
+//function addtwonum (number1,number2){ //--> parameters
+//    console.log(number1 + number2);
+//}
+//const result = addtwonum(3,4) // ---> arguments
+//console.log("Result: ", result);
+
+function addtwonum (number1,number2){
+   // let result = number1 + number2
+    //return result
+    //console.log('yogeeta'); //--> after return kuch nhi hota
+     return number1+number2
+}
+const result = addtwonum(3,8)
+console.log("result :",result);
