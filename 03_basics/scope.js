841b2de0fafe2c2f100d7fc8e69aef29d00d490e
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2024-06-02 13:54:11 +0000

    scope.js

---
+++ 03_basics/scope.js
@@ -0,0 +1,15 @@
+{}//....scope; come with functions, if-else statements etc,
+
+//var c = 300..GLOBAL SCOPE
+let a = 300
+if(true){
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+} // LOCAL SCOPE
+
+console.log(a);
