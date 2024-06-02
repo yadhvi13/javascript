511d51a25e5c6413534abf0aacd8a2c58f36dafa
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2024-06-02 14:28:40 +0000

    scope.js

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -5,11 +5,45 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
+    //console.log("INNER: ", a);
+   // for (let i = 0; i < array.length; i++) {
+    //    const element = array[i];
         
-    }
+   // }
 } // LOCAL SCOPE
 
-console.log(a);
+//console.log(a);
+
+// NESTED SCOPES
+function one(){
+    const username = "yogeeta"
+
+    function two(){
+        const website = "google"
+     //   console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+//one()
+
+if(true){
+    const username= "sammy"
+    if(username==="sammy"){
+        const website=" youtube"
+    //   console.log(username + website);
+    }
+  //  console.log(website); .. scope is global, hence not used
+}
+//console.log(username); 
+
+// EXAMPLE ********
+console.log(addone(5))
+function addone(value){
+    return value + 1
+}
+
+addtwo(5)
+const addtwo = function(num){
+    return num + 2
+}
