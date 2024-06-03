759d18664af08606348e431f9e5c4e477a60d135
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2024-06-03 02:26:57 +0000

    if-else.js

---
+++ 04_controlflow/if-else.js
@@ -0,0 +1,60 @@
+// IF-statement
+if(true){
+  // code will execute
+}
+if(false){
+ // code will not execute
+}
+
+const isuserloggedin = true
+const temp = 41
+if(temp === 41){
+   // console.log("less than 50");
+}
+else{
+   //console.log("temp is greater than 50");
+}
+/*
+const score = 200
+if(score > 100){
+    const power = 'fly'
+    console.log(`user power: ${power}`);
+}
+*/
+/*
+const balance = 1000
+//if (balance) console.log('test'); // implicit
+
+if (balance < 500) {
+    console.log("less than 500");
+}
+else if(balance < 750){
+    console.log("less than 750");
+}
+else if(balance < 900){
+    console.log("less than 1200");
+}
+else{
+    console.log("less than 1200");
+}
+*/
+/*
+const userloggedin = true
+const debitcard = true
+if(userloggedin && debitcard){
+    console.log("Allow to buy course");
+}
+*/
+/*
+const userloggedin = true
+const debitcard = true
+const loggedinfromGoogle = false
+const loggedinfromEmail = true
+
+if(userloggedin && debitcard){
+    console.log("Allow to buy course");
+}
+if (loggedinfromGoogle || loggedinfromEmail) {
+    console.log("User logged in");
+}
+*/
---
+++ 04_controlflow/switch.js
@@ -0,0 +1,29 @@
+/* 
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+
+const month = "march"
+switch (month){
+    case "jan":
+        console.log("january");
+        break;  
+    case "feb":
+        console.log("feburary");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+    default:
+        console.log("case match");
+        break;
+}
