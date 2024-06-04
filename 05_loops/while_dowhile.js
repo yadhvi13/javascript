852c9fe8311d7a044a06dd2570e6219fe6a6bc4a
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2024-06-04 02:34:14 +0000

    loop.js

---
+++ 05_loops/while_dowhile.js
@@ -0,0 +1,19 @@
+let index = 0
+while (index <= 10) {
+   // console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+
+let myarray = ["flash","batman","hanuman ji"]
+let arr = 0
+while (arr < myarray.length) {
+    //console.log(`value is ${myarray[arr]}`);
+    arr ++
+}
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score ++
+} while (score <= 10);
