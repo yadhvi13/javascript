422fef0719a1b5037f7b5f3576602a02613186eb
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2024-06-05 16:25:19 +0000

    filter.js

--- 05_loops/filter.js
+++ 05_loops/filter.js
@@ -68,3 +68,22 @@ let userbooks = books.filter( (bk)=> {
 //    return bk.publish >= 2004 && bk.genre==="history"
 //} )
 console.log(userbooks);
+
+
+
+const num = [1,2,3,4,5,6,7,8,9,10]
+
+//const newnums = num.map( (number)=>{
+//   return number + 10 
+//} )
+
+// CHAINING METHOD
+//const newnums = num.map().map().filter()
+
+const newnums = num
+              .map( (nums)=> nums*10)
+              .map( (nums)=> nums + 1 )
+              .filter( (nums)=> nums >= 40 )
+    
+console.log(newnums);
+
