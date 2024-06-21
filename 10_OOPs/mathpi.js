02d10062539836f2d6b242ca22ce12eb9c7ce339
yadhvi13 <147475124+yadhvi13@users.noreply.github.com>
2024-06-21 01:42:19 +0000

    interview question

--- 10_OOPs/mathpi.js
+++ 10_OOPs/mathpi.js
@@ -1,9 +1,9 @@
-//Object.getOwnPropertyDescriptor(Math)
+/*Object.getOwnPropertyDescriptor(Math)
 
-/*
 console.log(Math.PI);
 Math.PI = 7
 console.log(Math.PI);  */
+
 
 const obj = Object.getOwnPropertyDescriptor(Math, "PI")
 console.log(obj);
@@ -11,14 +11,23 @@ console.log(obj);
 const chai = {
     name : 'ginger chai',
     price: 250,
-    isAvailable : true
+    isAvailable : true,
+    orderChai : function(){
+        console.log('not working');
+    }
 }
 console.log(Object.getOwnPropertyDescriptor(chai,"isAvailable"));
-/*
-Object.defineProperty(chai,"isAvailable",{
-    writable : false,
-    enumerable : false,
+
+Object.defineProperty(chai, "name",{
+    //writable : false,
+    enumerable : true,
 })
 
-console.log(Object.getOwnPropertyDescriptor(chai,"isAvailable"));
-*/
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+
+for (let [key, value] of Object.entries(chai)) {
+    if(typeof value != 'function'){
+        console.log(`${key} : ${value}`);
+    }
+}
